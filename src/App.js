5d06b564f4c35ada29ae0b7de6334796d8d96dc5
pratik mane <pratik.mane1299@gmail.com>
2020-09-22 16:03:26 +0530

    Add loading state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [city, setCity] = useState('');
   const [weatherInfo, setWeatherInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setCity(e.target.value)
@@ -11,6 +12,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const locationRes = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`);
     const locationJson = await locationRes.json();
     const location = locationJson[0];
@@ -19,7 +21,7 @@ function App() {
     const weatherJson = await weatherRes.json();
 
     setWeatherInfo(weatherJson.consolidated_weather);
-
+    setLoading(false);
     setCity('');
   }
 
@@ -35,24 +37,28 @@ function App() {
           value={city} 
           onChange={handleChange} 
         />
-        <button className="button">Get Weather</button>
+        <button className="button">{!loading ? 'Get Weather' : 'Fetching weather data...'}</button>
       </form>
-      <div className="weather-wrapper">
-        {
-          weatherInfo.map(weather => (
-            <div className="weather-card" key={weather.id}>
-              <h4>{weather.applicable_date}</h4>
-              <img 
-                src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`} 
-                alt={weather.weather_state_name} 
-              />
-              <p><span>Humidity:</span> <span>{weather.humidity}°C</span></p>
-              <p><span>Min:</span> <span>{weather.min_temp.toFixed(2)}°C</span></p>
-              <p><span>Max:</span> <span>{weather.max_temp.toFixed(2)}°C</span></p>
-            </div>
-          ))
-        }
-      </div>
+      {
+        !loading && (
+          <div className="weather-wrapper">
+          {
+            weatherInfo.map(weather => (
+              <div className="weather-card" key={weather.id}>
+                <h4>{weather.applicable_date}</h4>
+                <img 
+                  src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`} 
+                  alt={weather.weather_state_name} 
+                />
+                <p><span>Humidity:</span> <span>{weather.humidity}°C</span></p>
+                <p><span>Min:</span> <span>{weather.min_temp.toFixed(2)}°C</span></p>
+                <p><span>Max:</span> <span>{weather.max_temp.toFixed(2)}°C</span></p>
+              </div>
+            ))
+          }
+          </div>
+        )
+      }
     </main>
   );
 }

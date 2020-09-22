b376fce599792cdf8c9ef0b085047133b523e844
pratik mane <pratik.mane1299@gmail.com>
2020-09-22 19:42:09 +0530

    Updates

---
+++ precache-manifest.9abb4d24cd27100917696f85770688e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce85d38eaab1b209a7a31440bc5e1cc",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "5ab550e31859b80399c0",
+    "url": "/WeatherApp/static/css/main.970ea3d7.chunk.css"
+  },
+  {
+    "revision": "054c48c5891dfc13c2f7",
+    "url": "/WeatherApp/static/js/2.2bc64c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.2bc64c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab550e31859b80399c0",
+    "url": "/WeatherApp/static/js/main.30abe972.chunk.js"
+  },
+  {
+    "revision": "f34a18f0592fbbc8ce7e",
+    "url": "/WeatherApp/static/js/runtime-main.dcd8b410.js"
+  }
+]);

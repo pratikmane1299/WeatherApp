8c007f63187c8af7bccf1169f9acff392762c354
pratik mane <pratik.mane1299@gmail.com>
2020-09-22 14:44:13 +0530

    Create city form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('');
+
+  const handleChange = (e) => {
+    setCity(e.target.value)
+  }
+
   return (
     <main>
       <h1>Weather App</h1>
+      <form className="form">
+        <label className="form-label">City</label>
+        <input 
+          type="text" 
+          name="city" 
+          className="input" 
+          value={city} 
+          onChange={handleChange} 
+        />
+        <button className="button">Get Weather</button>
+      </form>
     </main>
   );
 }

d0d9ed596fe40a30a8124352e0b0120b03464166
pratik mane <pratik.mane1299@gmail.com>
2020-09-22 14:57:20 +0530

    Fetch weather data for city on form submit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ function App() {
     setCity(e.target.value)
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const locationRes = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`);
+    const locationJson = await locationRes.json();
+    const location = locationJson[0];
+
+    const weatherRes = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${location.woeid}`);
+    const weatherJson = await weatherRes.json();
+    console.log(weatherJson);
+  }
+
   return (
     <main>
       <h1>Weather App</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <label className="form-label">City</label>
         <input 
           type="text" 
